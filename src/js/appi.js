fb9ac590ac51291a079e3e0c4692c327ff09da85
Andre29839 <133494235+Andre29839@users.noreply.github.com>
2023-08-04 10:00:13 +0300

    Add all functions

---
+++ src/js/appi.js
@@ -0,0 +1,29 @@
+import axios from 'axios';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const BASE_URL = 'https://pixabay.com/api/';
+const API_KEY = '38624276-36ed6cbc10c2af1663e372e7f';
+
+export const imgParams = {
+  q: '',
+  image_type: 'photo',
+  orientation: 'horizontal',
+  safesearch: true,
+  per_page: 40,
+  page: 1,
+};
+
+const customAxios = axios.create({
+  baseURL: `${BASE_URL}?key=${API_KEY}`,
+});
+
+export const getImages = async params => {
+  try {
+    const result = await customAxios.get('', { params });
+    return result;
+  } catch {
+    Notify.failure(
+      "We're sorry, but you've reached the end of search results."
+    );
+  }
+};
--- src/js/index.js
+++ src/js/index.js
@@ -1,67 +1,124 @@
-import SlimSelect from "slim-select";
-import { fetchBreeds, fetchCatByBreed } from "./cat-api.js";
-import Notiflix from "notiflix"
-import '/node_modules/slim-select/dist/slimselect.css';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { imgParams, getImages } from './appi.js';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 
 const refs = {
-    select: document.querySelector(".breed-select"),
-    loadingInProgress: document.querySelector(".loader"),
-    LoadingError: document.querySelector(".error"),
-    cardCat: document.querySelector(".cat-info"),
-    containerImg: document.querySelector(".curt-wraper")
+    form: document.querySelector('#search-form'),
+    gallery: document.querySelector('.gallery'),
+    scroll: document.querySelector('.scroll'),
+    options: {
+        root: null,
+        rootMargin: '500px',
+        threshold: 1.0,
+    },
 }
 
-fetchBreeds().then(data => {
-    refs.select.insertAdjacentHTML("beforeend", `<option value ="null" selected>Please select the breed</option>`)
-    
-    data.map(({ id, name }) => ({
-            value: id,
-            label: name,
-        })).forEach(e => {
-        refs.select.insertAdjacentHTML("beforeend", `<option value = "${e.value}">${e.label}</option>`)
-        refs.select.classList.remove("is-hidden")
-    })
-    new SlimSelect({
-        select: refs.select,
-    })
-   
-}).catch(err => {
-    console.log(err);
-    refs.LoadingError.classList.remove("is-hidden")
-}).finally(() => {
-    refs.loadingInProgress.classList.add("is-hidden")
-})
+const observer = new IntersectionObserver(onEntry, refs.options)
+let lightbox = new SimpleLightbox('.gallery a', { captionsData: "alt", captionDelay: 250 });
 
-refs.select.addEventListener("change", function (e) {
-    refs.cardCat.innerHTML = ""
-    refs.select.classList.add("is-hidden")
-    refs.loadingInProgress.classList.remove("is-hidden")
-    fetchCatByBreed(e.target.value).then(data => {
-        if (data.length == 0) {
-            Notiflix.Notify.failure("Oops! Something went wrong! Try reloading the page!")
-            return
-        }
-        let dataObj = {
-            url: data[0].url,
-            name: data[0].breeds[0].name,
-            description: data[0].breeds[0].description,
-            temperament: data[0].breeds[0].temperament,
-        }
-        makeElem(dataObj)
-    }).catch(err => {
-         Notiflix.Notify.failure("Oops! Something went wrong! Try reloading the page!")
-    }).finally(() => {
-        refs.loadingInProgress.classList.add("is-hidden")
-    })
-})
-
-const makeElem = ({ url, name, description, temperament }) => {
-    let card = `<img class="cat-image"  alt = "Cat Image" src="${url}" width="700px"></div>
-        <div class="cat-details">
-            <h2 class="breed-name">${name}</h2>
-            <p class="description">${description}</p>
-            <p class="temperament">${temperament}</p>`
-    refs.cardCat.innerHTML = card
-    refs.cardCat.classList.remove("is-hidden")
+function onEntry(entries) {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      getImages(imgParams).then(result => {
+          createGallery(result.data);
+      });
+    }
+  });
 }
 
+const createGallery = object => {
+        const totalHits = object.totalHits;
+        const hitsArray = object.hits;
+        if (hitsArray.length === 0) {
+            Notify.failure(
+                'Sorry, there are no images matching your search query. Please try again'
+            );
+        } else {
+            if (imgParams.page === 1) {
+                Notify.success(`Hooray! We found ${totalHits} images`);
+            }
+            markupResult(hitsArray, refs.gallery);
+            imgParams.page += 1;
+        }
+    };
+
+const markupResult = (array, container) => {
+        const markup = createMarkup(array);
+        container.insertAdjacentHTML('beforeend', markup);
+        lightbox.refresh();
+    };
+
+
+function createMarkup(images) {
+    console.log(images);
+  return images
+    .map(
+      ({
+        largeImageURL,
+        webformatURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => {
+        return `<a class="gallery-item" href="${largeImageURL}">
+  <div class="photo-card">
+    <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+    <div class="info">
+      <p class="info-item">
+        <b>Likes</b> ${likes}
+      </p>
+      <p class="info-item">
+        <b>Views</b> ${views}
+      </p>
+      <p class="info-item">
+        <b>Comments</b> ${comments}
+      </p>
+      <p class="info-item">
+        <b>Downloads</b> ${downloads}
+      </p>
+    </div>
+    </div>
+    </a>`;
+      }
+    )
+    .join('');
+}
+
+const onFormSubmit = evt => {
+        observer.disconnect();
+        evt.preventDefault();
+        imgParams.q = '';
+        imgParams.page = 1;
+        refs.gallery.innerHTML = '';
+        eventHandler(evt);
+    };
+
+refs.form.addEventListener('submit', onFormSubmit);
+
+const eventHandler = evt => {
+    if (evt.target.elements.searchQuery.value === '') {
+        Notify.info('Please, enter a word for search!');
+    } else {
+        imgParams.q = evt.target.elements.searchQuery.value;
+        getImages(imgParams).then(result => {
+            createGallery(result.data);
+            observer.observe(refs.scroll);
+        });
+    }
+}
+
+refs.form.elements.searchQuery.addEventListener("focus", onFocusInput)
+
+function onFocusInput(e) {
+    e.target.placeholder = "Search images..."
+}
+
+refs.form.elements.searchQuery.addEventListener("blur", onBlurInput)
+
+function onBlurInput(e) {
+    e.target.placeholder = ""
+}
